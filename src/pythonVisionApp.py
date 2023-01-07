a191dda078c89639eabc5d4495fdc3b351ab350f
FredProbst <github@fsd.probst.org>
2023-01-06 22:58:51 -0500

    commits the nearly finalized application
    Comments are still needed.

--- src/pythonVisionApp.py
+++ src/pythonVisionApp.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-#to search for a variable, click on the variable, hit escape, and type "#". Press "n" to move up and "N" to move down
 from cscore import CameraServer
 from networktables import NetworkTablesInstance
 from apriltag import apriltag
@@ -23,15 +22,15 @@ class CameraView(object):
         self.angleFromHoriz = angleFromHoriz
 
 
-class AprilTag(object):
-    def __init__(self, camera, coor):
-        #self.id = id
+class Target(object):
+    def __init__(self, camera, coor, id):
+        self.id = id
         self.normalizedY = (coor[1] - camera.height/2)/(camera.height/2) * -1
         self.normalizedX = (coor[0] - camera.width/2)/(camera.width/2)
         self.pitch = (self.normalizedY/2) * camera.vertFOV
         self.yaw = (self.normalizedX/2) * camera.horizFOV
         #(height of target (meters) - height of camera(meters))/tan(pitch + angle of camera)
-        self.distanceFromTarget = (camera.elevationOfTarget - camera.elevationOfCamera) / math.tan(math.radians(self.pitch + camera.angleFromHoriz))
+        self.distanceToTarget = (camera.elevationOfTarget - camera.elevationOfCamera) / math.tan(math.radians(self.pitch + camera.angleFromHoriz))
 
 
 
@@ -57,6 +56,12 @@ class VisionApplication(object):
         # Initialize configuration
         self.config = self.readConfig()
         self.team = self.config["team"]
+        #TODO: Fill out values below
+        #vertFOV = 
+        #horizFOV = 
+        #elevationOfTarget = 
+        #elevationOfCamera = 
+        #angleFromHoriz = 
         self.camera = CameraView(self.config['cameras'][0], 48.94175846, 134.3449419, 1.8288, 0.9779, 20.93552078)
 
         # Initialize Camera Server
@@ -109,7 +114,6 @@ class VisionApplication(object):
             if not notified[0]:
                 cond.wait()
 
-        # Insert your processing code here
         print("Connected!")
         
         self.vision_nt = ntinst.getTable('Shuffleboard/Vision')
@@ -124,13 +128,14 @@ class VisionApplication(object):
             # Notify output of error and skip iteration
             if frame_time1 == 0:
                 self.cvsrc.notifyError(self.sink.getError())
-                print("Error on line 184 with grabbing frame")
+                print("Error on line 126 with grabbing frame")
                 continue
 
 
             greys = cv2.cvtColor(input_img1, cv2.COLOR_BGR2GRAY)
             dets = self.detector.detect(greys)
-            # array of the coordinates of detected AprilTags
+
+            # array of Target classes
             targets = []
 
             for det in dets:
@@ -139,38 +144,22 @@ class VisionApplication(object):
                     cv2.polylines(input_img1, [rect], True, self.RED, 2)
                     ident = str(det["id"])
                     pos = det["center"].astype(int) + (-10,10)
-                    targets.append(AprilTag(self.camera,pos))
-                    #targets.append(pos)
-                    #cv2.putText(input_img1, ident, tuple(pos), self.FONT, 1, self.RED, 2)
+                    targets.append(Target(self.camera,pos,det["id"]))
+                    cv2.putText(input_img1, ident, tuple(pos), self.FONT, 1, self.RED, 2)
             
-            if not dets: 
+            if not targets: 
                 # If no apriltags are detected, targetDetected is set to false
                 self.vision_nt.putNumber('targetDetected',0)
             else: 
                 # If AprilTags are detected, targetDetected is set to true 
                 self.vision_nt.putNumber('targetDetected',1)
-                print(targets[0].id)
-                #normalizedY = (targets[0][1] - self.camera.height/2)/(self.camera.height/2) * -1
-                #print(targets[0][1])
-                #normalizedX = (targets[0][0] - self.camera.width/2)/(self.camera.width/2)
-                #self.vision_nt.putNumber('normalizedY', normalizedY) 
-                #self.vision_nt.putNumber('normalizedX', normalizedX) 
-                #pitch = (normalizedY/2) * 48.9417
-                #yaw = (normalizedX/2) * 134.3449
-                #self.vision_nt.putNumber('yaw', yaw) 
-                ##(height of target (m) - height of camera(m))/tan(pitch + angle of camera)
-                #self.distanceFromTarget = (1.8288- .9779) / math.tan(math.radians(pitch + 20.93552078))
-                #print(self.distanceFromTarget)
-                #offset = targets[0][0] - camCenter
-			    
-                #self.vision_nt.putNumber('offset',offset)
-                #self.vision_nt.putNumber('distanceFromTarget', self.distanceFromTarget)
+                #prints to the output on the web dashboard
+                print(self.camera.width)
+                self.vision_nt.putNumber('targetX',targets[0].normalizedX)
+                self.vision_nt.putNumber('targetY',targets[0].normalizedY)
+                self.vision_nt.putNumber('distanceToTarget',targets[0].distanceToTarget)
             
             self.cvsrc.putFrame(input_img1) 
-
-    #def calculateDistance(self, target):
-
-    #def normalizeCoordinates(self, coor):
 
 def main():
     visionApp = VisionApplication()
